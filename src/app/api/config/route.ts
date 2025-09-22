1cebc3277ae22ef23e5b3dc3cb03ef89f8ca90f4
ByCrilux-XMode <christian.estudia.sistemas@gmail.com>
2025-09-22 09:41:07 -0400

    link

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
   return Response.json({
-    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://contadjango-393159630636.europe-west1.run.app',
   });
 }
